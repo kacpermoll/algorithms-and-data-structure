195e0e258265757087f15a8885b13ab467c5ce2b
Kacper Moll <kacper.moll@gmail.com>
2021-12-16 11:51:03 +0100

    merge sorting

--- VI/sort_scalanie.py
+++ VI/sort_scalanie.py
@@ -1,14 +1,59 @@
-def mergeSort(T):
+def mergeSort(arr):
 
-    mid = len(T)//2
+    # if an array has more than one element
+    if len(arr)-1 > 0:
 
-    L = T[:mid]
-    R = T[mid:]
+        # assigning the middle element to the variable mid
+        mid = len(arr)//2
 
-    mergeSort(L)
-    mergeSort(R)
+        # dividing an array into two parts using the middle element
+        L = arr[:mid]
+        R = arr[mid:]
 
-    merge(T, L, R)
+        # recursive execution of functions for both parts of the array
+        mergeSort(L)
+        mergeSort(R)
+
+        # merging an array using the merge function
+        merge(arr, L, R)
 
 
-def merge(T, L, R):
+def merge(arr, L, R):
+
+    i = j = k = 0
+
+    # as long as L and R have elements
+    # sorting takes place, the smaller element is written into the arr array
+    while i < len(L) and j < len(R):
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            k += 1
+            i += 1
+        else:
+            arr[k] = R[j]
+            k += 1
+            j += 1
+
+    # if there are any elements left in any of the arrays, they should be added to arr
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+
+def main():
+    tab = [4, 9, 12, 2, 1, 7, 16, 3]
+    print("Przed sortowaniem:")
+    print(tab)
+    mergeSort(tab)
+    print("Po:")
+    print(tab)
+
+
+if __name__ == '__main__':
+    main()
